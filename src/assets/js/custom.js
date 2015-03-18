29f34d5fd4fbca1f7c591c6f07753e6695112472
felipe <feliperocha.ti@gmail.com>
2015-03-18 18:24:38 -0300

    add assets folder in src

---
+++ src/assets/js/custom.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    // SCROLL SCRIPTS 
+        $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000, 'easeInOutQuad');
+            event.preventDefault();
+        });
+});
+
+//Google Maps Init
+function init_map() {
+    var var_location = new google.maps.LatLng(-22.83395,-45.22665);
+
+    var var_mapoptions = {
+      center: var_location,
+      zoom: 14
+  };
+
+  var var_marker = new google.maps.Marker({
+    position: var_location,
+    map: var_map,
+    title:"Betaup"});
+
+  var var_map = new google.maps.Map(document.getElementById("map-container"),
+    var_mapoptions);
+
+  var_marker.setMap(var_map); 
+
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
